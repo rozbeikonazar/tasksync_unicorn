b37663242ce3747ae6a3006124d9b7f618992b2b
Nazar <root1@Nazars-MacBook-Air.local>
2024-04-09 21:26:17 +0200

    feat: add and initialize necessary packages

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const users = require('./routes/users');
+const session = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+require('./strategies/local-strategy');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const COOKIE_SECRET = process.env.COOKIE_SECRET || "cookie_secret";
+const SESSION_SECRET = process.env.SESSION_SECRET || "session_secret";
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+mongoose
+  .connect('mongodb://localhost/task_sync')
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(`Error: ${err}`));
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(COOKIE_SECRET));
+app.use(session({
+  secret: SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 60000 * 60,
+  },
+})
+)
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/api/user', users.router);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(function (err, req, res, next) {
+  console.error(err);
+  // Send a JSON response with the error message
+  res.status(err.status || 500).json({ error: err.message });
+ });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
