517ab4d9028e0af187d581c0172ec3f0363e1ebf
Nazar <root1@Nazars-MacBook-Air.local>
2024-04-10 17:53:44 +0200

    feat: add schema for m2m relationship

--- mongoose/schemas/workspace.js
+++ mongoose/schemas/workspace.js
@@ -1,15 +1,35 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 const WorkspaceSchema = new mongoose.Schema({
     name: {
         type: mongoose.Schema.Types.String,
-        required: true
+        required: true,
     },
     creator_id: {
-        type: mongoose.Schema.ObjectId, ref: "User",
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+});
+
+const UserWorkspaceInvintationSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    workspace_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Workspace',
         required: true
     }
 })
 
-const Workspace = mongoose.model('Workspace')
-module.exports = Workspace;
+const Workspace = mongoose.model('Workspace', WorkspaceSchema)
+const UserWorkspaceInvintation = mongoose.model('UserWorkspaceInvintation', UserWorkspaceInvintationSchema)
+
+module.exports = {
+    Workspace,
+    UserWorkspaceInvintation
+};
