9a728b192ef7c768d42c5fdba57bac2f590c577a
Nazar <root1@Nazars-MacBook-Air.local>
2024-04-11 21:56:21 +0200

    feat: add cascade functionality for related objects

--- mongoose/schemas/user.js
+++ mongoose/schemas/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const {Workspace} = require("./workspace.js")
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -16,6 +17,19 @@ const UserSchema = new mongoose.Schema({
         required: true
     },
 });
+UserSchema.pre('deleteOne', async function (next) {
+    try {
+        let id = this.getQuery()["_id"];        
+        await Workspace.deleteMany({creator_id: id});
+        next();
+    }
+    catch (err) {
+        next(err);
+
+    }
+});
+
+
 UserSchema.index({ username: 1 }, { unique: true, collation: { locale: 'en', strength: 2 } });
 
 const User = mongoose.model('User', UserSchema)
