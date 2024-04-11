7d7941966f9c96edb239a0ec15f084f458b49383
Nazar <root1@Nazars-MacBook-Air.local>
2024-04-11 21:54:15 +0200

    feat: add /delete_account route

--- routes/users.js
+++ routes/users.js
@@ -4,21 +4,14 @@ const {hashPassword} = require('../helpers/users.js')
 const {registerUserValidationSchema} = require('../helpers/validationSchemas.js')
 const {validationResult , checkSchema, matchedData} = require('express-validator');
 const passport = require('passport')
-const User = require('../mongoose/schemas/user.js')
+const User = require('../mongoose/schemas/user.js');
+const checkAuth = require('../middlewares/checkAuth.js');
 
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
   res.status(200).json({ message: 'Login successful' });
 });
 
-
-// router.get('/auth', (req, res) => {
-//   if (req.user) {
-//     return res.send(req.user)
-//   }
-//   return res.sendStatus(401)
-// })
- 
 router.post('/logout', (req,res) => {
   if (!req.user) {
     return res.sendStatus(401);
@@ -53,6 +46,30 @@ router.post('/register', checkSchema(registerUserValidationSchema), async (req, 
   }
 });
 
+
+router.delete('/delete_account', checkAuth, async(req, res, next) => {
+  user_id = req.user._id
+  try {
+    const result = await User.deleteOne({_id: user_id})
+    if (!result) {
+      res.status(500).json({ error: "User deletion failed." });
+  }
+    req.logout((err) => {
+      if (err) {
+        return res.sendStatus(400);
+      }
+      return res.status(200).json({ message: `User deleted successfully.`});
+    })
+
+  }
+
+  catch (err) {
+      console.error("Error deleting User:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    
+  }
+
+})
 
 module.exports = {
   router
