bdd8986000539d6950ab3fc6761bfe481793636f
Nazar <root1@Nazars-MacBook-Air.local>
2024-04-13 00:26:38 +0200

    feat: add on delete cascade functionality for Comment

--- mongoose/schemas/task.js
+++ mongoose/schemas/task.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const Comment = require("./comment.js")
 
 const TaskSchema = new mongoose.Schema({
     name: {
@@ -31,6 +31,33 @@ const TaskSchema = new mongoose.Schema({
     }
 
 })
+
+TaskSchema.pre("deleteOne", async function(next) {
+    const taskID =  this.getQuery()["_id"]
+    try {
+        await Comment.deleteMany({task_id: taskID})
+        next()
+    } catch (err) {
+        next(err);
+    }
+    
+})
+
+TaskSchema.pre("deleteMany", async function(next) {
+    const workspaceIDs = this.getQuery().workspace_id['$in'];    
+    try {
+        const tasks = await Task.find({workspace_id: {$in : workspaceIDs}})
+        const taskIDs = tasks.map(task => task._id)
+        console.log("TaskIDS: ", taskIDs)
+        await Comment.deleteMany({task_id: {$in: taskIDs}})
+        
+        next()
+    } catch (err) {
+        next(err);
+    }
+
+
+} )
 
 const Task = mongoose.model('Task', TaskSchema);
 
