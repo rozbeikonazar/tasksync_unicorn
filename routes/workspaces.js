63d1999eefa35247ce007856a88dbb76ec48ef9f
Nazar <root1@Nazars-MacBook-Air.local>
2024-04-13 19:09:53 +0200

    fix: errors with return + remove unnecessary console logs

--- routes/workspaces.js
+++ routes/workspaces.js
@@ -40,16 +40,15 @@ async function(req, res){
 })
 
 router.get("/", checkAuth, async function(req, res){
-    // returns all workspaces assigned to user
+    // Returns all workspaces assigned to user
     try {
         const userID = req.user._id
         const workspacesCreator = await Workspace.find({creator_id: userID})
         const workspaceInvitations = await UserWorkspaceInvintation.find({user_id: userID}).populate('workspace_id')
         const workspacesInvited = workspaceInvitations.map(invitation => invitation.workspace_id);
-
         // Merge both arrays to get all workspaces the user belongs to
         const allWorkspaces = [...workspacesCreator, ...workspacesInvited];
-        res.status(200).json(allWorkspaces);
+        return res.status(200).json(allWorkspaces);
     }
     catch (err){
         return res.status(500).json({ error: "Internal Server Error" });
@@ -65,9 +64,9 @@ async function (req,res) {
     try {
         const updatedWorkspace = await Workspace.findByIdAndUpdate(workspaceID, {$set: data}, {new: true});
         if (!updatedWorkspace) {
-            res.status(404).json({error: "Workspace not found"})
+            return res.status(404).json({error: "Workspace not found"})
         }
-        res.status(200).json(updatedWorkspace)
+        return res.status(200).json(updatedWorkspace)
     }
     catch (err) {
         if (err.name === 'ValidationError') {
@@ -81,14 +80,13 @@ router.delete("/:workspaceID", checkAuth, isWorkspaceCreator, async function(req
     try {
         const result = await Workspace.deleteOne({_id: req.workspace._id});
         if (!result) {
-            res.status(422).json({ error: "Workspace deletion failed." });
+            return res.status(422).json({ error: "Workspace deletion failed." });
         }
-        res.status(200).json({ message: `Workspace deleted successfully.`});
+        return res.status(200).json({ message: `Workspace deleted successfully.`});
 
         
     } catch (err) {
-        console.error("Error deleting workspace:", err);
-        res.status(500).json({ error: "Internal Server Error" });
+        return res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
@@ -103,12 +101,12 @@ router.delete("/:workspaceID/quit", checkAuth, async function(req, res) {
     const result = await UserWorkspaceInvintation.deleteOne({user_id: userID, workspace_id: workspaceID})
 
     if (!result) {
-        res.status(400).json({error: "Could not quit from workspace or you are not a member of this workspace"})
+        return res.status(400).json({error: "Could not quit from workspace or you are not a member of this workspace"})
     }
-    res.status(200).json({message: 'Sucessfully quit from workspace'})
+    return res.status(200).json({message: 'Sucessfully quit from workspace'})
 
     } catch (err) {
-        res.status(500).json({error: "Internal Server Error"});
+        return res.status(500).json({error: "Internal Server Error"});
     }
 
 }) 
@@ -116,21 +114,22 @@ router.delete("/:workspaceID/quit", checkAuth, async function(req, res) {
 router.delete("/:workspaceID/kick/:userID", checkAuth, isWorkspaceCreator, async function(req,res) {
     userID = req.params.userID
     const workspaceID = req.params.workspaceID;
+    try {
     const result = await UserWorkspaceInvintation.deleteOne({user_id: userID, workspace_id: workspaceID});
     if (!result) {
-        res.status(400).json({error: "Could not kick a user or he is not a member of workspace"})
+        return res.status(400).json({error: "Could not kick a user or he is not a member of workspace"})
     }
-    res.status(200).json({message: "Sucessfully kicked user from workspace"})
-
+    return res.status(200).json({message: "Sucessfully kicked user from workspace"})
+    } catch (err) {
+        return res.status(500).json("Internal Server Error")
+    }
 })
 
 
 router.post('/:workspaceID/generate_invintation',
     isWorkspaceCreator, async function(req, res) {
-        const invitationLink = `${HOST}${WORKSPACE_PREFIX}${uuidv4()}/join`;        
-        
+        const invitationLink = `${HOST}${WORKSPACE_PREFIX}${uuidv4()}/join`;
         try {
-            
             const newInvintation = new Invintation({
                 url: invitationLink,
                 workspace_id: req.workspace._id
@@ -139,8 +138,7 @@ router.post('/:workspaceID/generate_invintation',
             return res.status(201).json({"link": savedLink})
         }
         catch (err){
-            console.log(err);
-            res.status(500).json({error: "Internal Server Error"});
+            return res.status(500).json({error: "Internal Server Error"});
         }
     }
 )
@@ -170,14 +168,11 @@ router.post('/:uuid/join', checkAuth, async function(req, res) {
                 workspace_id: invitation.workspace_id
             })
 
-            const joinToWorkspace = await newJoin.save({
-    
-        })
+            const joinToWorkspace = await newJoin.save()
         // This invintation links are one-time, so after user joined to a workspace
         // this link should be deleted
         const result = await Invintation.deleteOne(invitation._id)
         if (!result) {
-            console.error(`Invintation with ID: ${invitation._id} was not deleted`)
             return res.status(500).json({error: "Internal server error"})
         }
         return res.status(200).json(joinToWorkspace)
@@ -223,7 +218,7 @@ router.get('/:workspaceID/tasks', checkAuth, checkWorkspaceAccess, async functio
     const {workspaceID} = req.params
     try {
         const tasks = await Task.find({workspace_id: workspaceID})
-        res.status(200).json(tasks)
+        return res.status(200).json(tasks)
     }
     catch(err) {
         return res.status(500).json({ error: "Internal Server Error" });
@@ -237,7 +232,7 @@ router.get('/:workspaceID/tasks/:taskID', checkAuth, checkWorkspaceAccess, async
     const {workspaceID, taskID} = req.params
     try {
         const task = await Task.findOne({_id: taskID, workspace_id: workspaceID})
-        res.status(200).json(task)
+        return res.status(200).json(task)
 
     }
     catch(err) {
@@ -257,7 +252,7 @@ router.put('/:workspaceID/tasks/:taskID',
         if (!updatedTask) {
             return res.status(404).json({ error: "Task not found" });
         }
-        res.status(200).json(updatedTask);
+        return res.status(200).json(updatedTask);
     } catch (err) {
         if (err.name === 'ValidationError') {
             return res.status(422).json({ error: "Validation failed" });
@@ -272,12 +267,12 @@ router.delete('/:workspaceID/tasks/:taskID', checkAuth, checkWorkspaceAccess, as
     try {
         const result = await Task.deleteOne({_id: taskID})
         if (!result) {
-            res.status(422).json({error: "Task deletion failed"})
+            return res.status(422).json({error: "Task deletion failed"})
         }
-        res.status(200).json({ message: `Task deleted successfully.`});
+        return res.status(200).json({ message: `Task deleted successfully.`});
     
     } catch(err){
-        res.status(500).json({error: "Internal Server Error"})
+        return res.status(500).json({error: "Internal Server Error"})
 
     }
 })
@@ -312,7 +307,7 @@ router.get('/:workspaceID/tasks/:taskID/comments', checkAuth, checkWorkspaceAcce
     const {taskID} = req.params
     try {
         const comments = await Comment.find({task_id: taskID})
-        res.status(200).json(comments)
+        return res.status(200).json(comments)
     } catch(err) {
         return res.status(500).json({error: "Internal Server Error"});
     }
@@ -326,12 +321,12 @@ async function(req, res) {
     try {
         const result = await Comment.deleteOne({_id: commentID})
         if (!result) {
-            res.status(422).json({error: "Comment deletion failed"})
+            return res.status(422).json({error: "Comment deletion failed"})
         }
-        res.status(200).json({message: "Comment deleted successfully"})
+        return res.status(200).json({message: "Comment deleted successfully"})
     }
     catch(err) {
-        res.status(500).json({error: "Internal Server Error"})
+        return res.status(500).json({error: "Internal Server Error"})
 
     }
 
