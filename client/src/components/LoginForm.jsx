bc2601f51b69b54ba36a0c00cadd1bea4d0c5716
Nazar <root1@Nazars-MacBook-Air.local>
2024-05-03 14:24:56 +0200

    test: changed links to backend

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -30,7 +30,7 @@ export function LoginForm() {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/api/user/login", {
+      const response = await fetch("https://tasksync-unicorn-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
