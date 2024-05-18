a5c579f7163544d082bb83cf9502b16e76fd6ff9
Polina <hutskevichpolina@gmail.com>
2024-05-18 21:33:18 +0200

    feat: add docker

--- api/app.js
+++ api/app.js
@@ -1,56 +1,56 @@
-const express = require('express');
-const logger = require('morgan');
-const users = require('./routes/users');
-const workspaces = require('./routes/workspaces')
-const session = require('express-session');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('./strategies/local-strategy');
+const express = require("express");
+const logger = require("morgan");
+const users = require("./routes/users");
+const workspaces = require("./routes/workspaces");
+const session = require("express-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("./strategies/local-strategy");
 
 const COOKIE_SECRET = process.env.COOKIE_SECRET || "cookie_secret";
 const SESSION_SECRET = process.env.SESSION_SECRET || "session_secret";
-const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || 'http://localhost:5173';
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:5173";
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/tasksync";
 const app = express();
 
 mongoose
-  .connect('mongodb://localhost/task_sync')
+  .connect(mongoURI)
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(`Error: ${err}`));
 
+app.use(
+  cors({
+    origin: FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: FRONTEND_ORIGIN, 
-  credentials: true,
-}));
-
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser(COOKIE_SECRET));
-app.use(session({
-  secret: SESSION_SECRET,
-  saveUninitialized: false,
-  resave: false,
-  cookie: {
-    maxAge: 60000 * 60,
-    sameSite: 'none',
-    secure: false
-  },
-})
-)
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+      sameSite: "none",
+      secure: false,
+    },
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/api/user', users.router);
-app.use('/api/workspaces', workspaces.router)
+app.use("/api/user", users.router);
+app.use("/api/workspaces", workspaces.router);
 
 app.use(function (err, req, res, next) {
   console.error(err);
   // Send a JSON response with the error message
   res.status(err.status || 500).json({ error: err.message });
- });
-
-
+});
 
 module.exports = app;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +10,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-})
+  server: {
+    host: true,
+  },
+});
