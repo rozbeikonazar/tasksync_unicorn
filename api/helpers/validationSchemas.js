d6972d894c9caf0c55e76e6decfab8d151f7c7e0
Nazar <qwerty2004123@gmail.com>
2024-06-21 08:27:40 +0200

    fix bug

--- api/helpers/validationSchemas.js
+++ api/helpers/validationSchemas.js
@@ -1,142 +1,143 @@
 const registerUserValidationSchema = {
-	username: {
-		isLength: {
-			options: {
-				min: 5,
-				max: 32,
-			},
-			errorMessage:
-				"Username must be at least 5 characters with a max of 32 characters",
-		},
-		notEmpty: {
-			errorMessage: "Username cannot be empty",
-		},
-		isString: {
-			errorMessage: "Username must be a string!",
-		},
-	},
-	display_name: {
-		notEmpty: {
-            errorMessage: "Display Name cannot be empty"
-        },
-	},
-	password: {
-				isLength: {
-			options: {
-				min: 8,
-				max: 32,
-			},
-			errorMessage:
-				"Password must be at least 8 characters with a max of 32 characters",
-		},
-		notEmpty: {
-			errorMessage: "Password cannot be empty",
-		},
-		isString: {
-			errorMessage: "Password must be a string!",
-		},
-	},
+  username: {
+    isLength: {
+      options: {
+        min: 5,
+        max: 32,
+      },
+      errorMessage:
+        "Username must be at least 5 characters with a max of 32 characters",
+    },
+    notEmpty: {
+      errorMessage: "Username cannot be empty",
+    },
+    isString: {
+      errorMessage: "Username must be a string!",
+    },
+  },
+  display_name: {
+    notEmpty: {
+      errorMessage: "Display Name cannot be empty",
+    },
+  },
+  password: {
+    isLength: {
+      options: {
+        min: 8,
+        max: 32,
+      },
+      errorMessage:
+        "Password must be at least 8 characters with a max of 32 characters",
+    },
+    notEmpty: {
+      errorMessage: "Password cannot be empty",
+    },
+    isString: {
+      errorMessage: "Password must be a string!",
+    },
+  },
 };
 
 const workspaceValidationSchema = {
-    name: {
-        isString: {
-            errorMessage: "Workspace name must be a string!"
-        },
-        notEmpty: {
-            errorMessage: "Workspace name cannot be empty"
-        },
-        isLength: {
-            options: {
-                max: 100
-            },
-            errorMessage: "Workspace name must have a maximum length of 100 characters",
-        }
-    }
+  name: {
+    isString: {
+      errorMessage: "Workspace name must be a string!",
+    },
+    notEmpty: {
+      errorMessage: "Workspace name cannot be empty",
+    },
+    isLength: {
+      options: {
+        max: 100,
+      },
+      errorMessage:
+        "Workspace name must have a maximum length of 100 characters",
+    },
+  },
 };
 
 const createTaskValidationSchema = {
-    name: {
-        notEmpty: {
-            errorMessage: 'Task name is required',
-        },
-		isString: {
-            errorMessage: "Task name must be a string!"
-        },
+  name: {
+    notEmpty: {
+      errorMessage: "Task name is required",
     },
-    description: {
-        optional: true,
-		isString: {
-			errorMessage: 'Task description must be a string',
-		},
+    isString: {
+      errorMessage: "Task name must be a string!",
     },
-    deadline: {
-        optional: true,
-        isISO8601: {
-            errorMessage: 'Invalid deadline format',
-        },
-        toDate: true,
+  },
+  description: {
+    optional: true,
+    isString: {
+      errorMessage: "Task description must be a string",
     },
-    status: {
-        optional: true,
-        isIn: {
-            options: [['Done', 'In progress', 'Not started']],
-            errorMessage: 'Invalid status',
-        },
+  },
+  deadline: {
+    optional: true,
+    isISO8601: {
+      errorMessage: "Invalid deadline format",
     },
+    toDate: true,
+  },
+  status: {
+    optional: true,
+    isIn: {
+      options: [["Done", "In progress", "Not started"]],
+      errorMessage: "Invalid status",
+    },
+  },
 };
 
 const updateTaskValidationSchema = {
-    name: {
-		isString: {
-            errorMessage: "Task name must be a string!"
-        },
-		optional: true,
+  name: {
+    isString: {
+      errorMessage: "Task name must be a string!",
     },
-    description: {
-        optional: true,
-		isString: {
-			errorMessage: 'Task description must be a string',
-		},
+    optional: true,
+  },
+  description: {
+    optional: true,
+    isString: {
+      errorMessage: "Task description must be a string",
     },
-    deadline: {
-        optional: true,
-        isISO8601: {
-            errorMessage: 'Invalid deadline format',
-        },
-        toDate: true,
+  },
+  // deadline: {
+  //     optional: true,
+  //     isISO8601: {
+  //         errorMessage: 'Invalid deadline format',
+  //     },
+  //     toDate: true,
+  // },
+  status: {
+    optional: true,
+    isIn: {
+      options: [["Done", "In progress", "Not started"]],
+      errorMessage: "Invalid status",
     },
-    status: {
-        optional: true,
-        isIn: {
-            options: [['Done', 'In progress', 'Not started']],
-            errorMessage: 'Invalid status',
-        },
-    },
+  },
 };
 
 const commentValidationSchema = {
-    content: {
-        isString: {
-            errorMessage: "Comment content must be a string",
-        },
-        notEmpty: {
-            errorMessage: "Comment content cannot be empty"
-        },
-        isLength: {
-            options: {
-                max: 300
-            },
-            errorMessage: "Comment content must have a maximum length of 300 characters",
-        }
-    }
-}
-
+  content: {
+    isString: {
+      errorMessage: "Comment content must be a string",
+    },
+    notEmpty: {
+      errorMessage: "Comment content cannot be empty",
+    },
+    isLength: {
+      options: {
+        max: 300,
+      },
+      errorMessage:
+        "Comment content must have a maximum length of 300 characters",
+    },
+  },
+};
 
 module.exports = {
-	registerUserValidationSchema, 
-	workspaceValidationSchema,
-	createTaskValidationSchema,
-	updateTaskValidationSchema,
-    commentValidationSchema,
-}
+  registerUserValidationSchema,
+  workspaceValidationSchema,
+  createTaskValidationSchema,
+  updateTaskValidationSchema,
+  commentValidationSchema,
+};
